a9405add35649843da7be8fc820ee9900167cec8
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 17:42:29 -0300

    feat: add margin bottom

--- src/components/ContainerPage.tsx
+++ src/components/ContainerPage.tsx
@@ -7,7 +7,7 @@ interface ContainerPageProps {
 
 function ContainerPage({ children, title }: ContainerPageProps) {
     return (
-        <div className="w-full flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center mb-20">
             <Header title={title} />
             {children}
         </div>
