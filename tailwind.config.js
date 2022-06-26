61b14d2226869576a9de06dde4036a05467eb9cd
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 11:55:09 -0300

    feat: add color pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
     content: ['./src/**/*.{html,tsx,jsx,ts,js}'],
     theme: {
+        colors: {
+            transparent: 'transparent',
+            c_primary: '#FE3E6D',
+            c_secondary: '#1A93DA',
+            c_secondary_light: '#EAF7FF',
+            c_grayscale: '#3B3B3B',
+        },
         screens: {
             sm: '640px',
             md: '768px',
