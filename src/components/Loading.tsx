79fb9b1cf40af5b0c79cc58d5a6a4c77ffa771a4
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 13:51:17 -0300

    feat: create loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,10 @@
+import ReactLoading from 'react-loading';
+
+function Loading() {
+    return (
+        <div className="w-full my-24 flex justify-center items-center">
+            <ReactLoading type="spin" color="#FE3E6D" />
+        </div>
+    );
+}
+export default Loading;
