66834eefa2ff0eed6a9bbd0b1e267bfd0681e4cf
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 01:27:32 -0300

    feat: consuming api with basic implementation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,7 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
 import { useAuth } from '../data-access/auth/AuthContext';
+import { IExtract } from '../interfaces/IExtract';
 import '../styles/App.css';
 
 function Home() {
@@ -8,14 +11,40 @@ function Home() {
         MakeLogout();
     }
 
+    const [extractList, setExtractList] = useState<IExtract[]>([]);
+
+    useEffect(() => {
+        api.get(`/results`).then(response => {
+            setExtractList(response.data);
+        });
+    }, []);
+
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="w-full flex flex-col justify-center items-center">
             <button
                 className="bg-gray-100 p-3 rounded-md hover:bg-gray-400 transition-colors mt-10"
                 onClick={handleLogout}
             >
                 Logout
             </button>
+            {extractList.map(extract => (
+                <div className="w-full border-blue-500 border">
+                    <div>{extract.amountTotal}</div>
+                    <div>{extract.date}</div>
+                    {extract.items.map(item => (
+                        <div>
+                            <p>{item.actor}</p>
+                            <p>{item.amount}</p>
+                            <p>{item.dateEvent}</p>
+                            <p>{item.entry}</p>
+                            <p>{item.scheduled}</p>
+                            <p>{item.source}</p>
+                            <p>{item.status}</p>
+                            <p>{item.type}</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
     );
 }
