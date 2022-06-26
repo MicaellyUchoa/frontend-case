a69c81c5b0308daf679808a366f5b0b00922bb3f
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 18:33:57 -0300

    feat: same day verification in format date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,15 +1,19 @@
+import { isSameDay } from 'date-fns';
 import format from 'date-fns/format';
 import ptBR from 'date-fns/locale/pt-BR';
 
 function formatDate(date: string, formatMode: string) {
     const dateForFormat = new Date(date);
 
-    return format(
-        new Date(new Date(dateForFormat.valueOf() + dateForFormat.getTimezoneOffset() * 60 * 1000)),
-        formatMode,
-        {
-            locale: ptBR,
-        },
+    return (
+        (isSameDay(new Date(), dateForFormat) ? 'Hoje - ' : '') +
+        format(
+            new Date(new Date(dateForFormat.valueOf() + dateForFormat.getTimezoneOffset() * 60 * 1000)),
+            formatMode,
+            {
+                locale: ptBR,
+            },
+        )
     );
 }
 
