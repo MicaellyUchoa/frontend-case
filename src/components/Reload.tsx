da5280bab7c64a67c8b1e686080d1df3fb995d88
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-27 00:52:01 -0300

    fix: change message reload

--- src/components/Reload.tsx
+++ src/components/Reload.tsx
@@ -8,7 +8,7 @@ function Reload({ setReload }: ReloadProps) {
     return (
         <div className="w-full my-24 flex flex-col gap-10 justify-center items-center">
             <p className="text-c_grayscale_medium text-lg text-center">
-                Não encontramos por aqui, tente novamente mais tarde!
+                Não encontramos nada por aqui, tente novamente mais tarde!
             </p>
             <button className="bg-c_primary text-white p-4 rounded-xl" onClick={() => setReload(true)}>
                 Regarregar
