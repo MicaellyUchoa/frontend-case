d2cd474bb4da197b842dd1b9eb67d25fe23c4027
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-26 22:50:15 -0300

    refactor: semantic html

--- src/components/ContainerPage.tsx
+++ src/components/ContainerPage.tsx
@@ -9,7 +9,7 @@ function ContainerPage({ children, title }: ContainerPageProps) {
     return (
         <div className="w-full flex flex-col justify-center items-center mb-20">
             <Header title={title} />
-            <div className="w-full flex-wrap px-8 md:px-6 lg:px-36 py-5">{children}</div>
+            <main className="w-full flex-wrap px-8 md:px-6 lg:px-36 py-5">{children}</main>
         </div>
     );
 }
