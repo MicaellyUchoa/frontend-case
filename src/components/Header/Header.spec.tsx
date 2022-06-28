943f0862c9b11bd80337ad4811431107fde02b4d
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-28 10:54:57 -0300

    test: add unit test on header component

---
+++ src/components/Header/Header.spec.tsx
@@ -0,0 +1,28 @@
+import { render, RenderResult, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { IHeaderProps } from '../../interfaces/IHeaderProps';
+
+let props: IHeaderProps;
+let component: RenderResult<typeof import('@testing-library/dom/types/queries'), HTMLElement, HTMLElement>;
+import Header from './index';
+
+beforeEach(() => {
+    props = {
+        title: 'Extrato',
+    };
+    jest.mock('../../data-access/auth/AuthContext', () => () => {});
+
+    component = render(<Header {...props} />);
+});
+describe('Header Component', () => {
+    test('should render with title', async () => {
+        expect(component.getByText('Extrato')).toBeTruthy();
+    });
+
+    test('should click on logout button', async () => {
+        const user = userEvent.setup();
+
+        await user.click(component.getByTestId('logout'));
+    });
+});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
-import { useAuth } from '../data-access/auth/AuthContext';
+import { useAuth } from '../../data-access/auth/AuthContext';
 import { TbLogout } from 'react-icons/tb';
-import { IHeaderProps } from '../interfaces/IHeaderProps';
+import { IHeaderProps } from '../../interfaces/IHeaderProps';
 
 function Header({ title }: IHeaderProps) {
     const { MakeLogout } = useAuth();
@@ -9,7 +9,7 @@ function Header({ title }: IHeaderProps) {
         <header className="flex justify-between w-full items-center px-8 md:px-6 lg:px-36  py-5 border-b-2 border-gray-100">
             <p className="text-xl font-black">{title}</p>
 
-            <button className="flex gap-2 outline-none" onClick={MakeLogout}>
+            <button data-testid="logout" className="flex gap-2 outline-none" onClick={MakeLogout}>
                 <p className="text-sm font-medium">sair</p>
                 <TbLogout className="cursor-pointer" size={20} />
             </button>
