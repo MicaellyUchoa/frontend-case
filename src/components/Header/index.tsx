23408e45b7f3370c54ee6a8dafb211b202da1b68
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-28 18:47:52 -0300

    feat: add focus and hover on logout button

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,9 +9,14 @@ function Header({ title }: IHeaderProps) {
         <header className="flex justify-between w-full items-center px-8 md:px-6 lg:px-36  py-5 border-b-2 border-gray-100">
             <p className="text-xl font-black">{title}</p>
 
-            <button data-testid="logout" className="flex gap-2 outline-none" onClick={MakeLogout}>
-                <p className="text-sm font-medium">sair</p>
-                <TbLogout className="cursor-pointer" size={20} />
+            <button data-testid="logout" className="group flex gap-2 outline-none" onClick={MakeLogout}>
+                <p className="text-sm font-medium group-hover:text-c_grayscale_medium group-focus:text-c_grayscale_medium">
+                    sair
+                </p>
+                <TbLogout
+                    className="cursor-pointer group-hover:text-c_grayscale_medium group-focus:text-c_grayscale_medium"
+                    size={20}
+                />
             </button>
         </header>
     );
