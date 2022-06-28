983314bfe41003788fd691b3dfc8003dd4d9900f
MicaellyUchoa <micaellyit@hotmail.com>
2022-06-28 10:55:47 -0300

    style: order archives on use extract filteres hook

---
+++ src/hooks/useExtractFiltered/index.ts
@@ -0,0 +1,47 @@
+import { IExtract } from '../../interfaces/IExtract';
+import { IUseExtractFilteredProps } from '../../interfaces/IUseExtractFilteredProps';
+import { useFilterCondition } from '../useFilterCondition';
+
+//TODO test here
+function useExtractFiltered({
+    statusItems,
+    search,
+    extractList,
+    setFilteredExtractList,
+}: IUseExtractFilteredProps): IExtract[] {
+    let provisionalExtract = [] as IExtract[];
+
+    search &&
+        extractList.map(extractItem => {
+            let provisional: IExtract;
+            provisional = {
+                amountTotal: extractItem.amountTotal,
+                date: extractItem.date,
+                items: extractItem.items.filter(item => {
+                    return item.actor.toLowerCase().includes(search.toLowerCase());
+                }),
+            };
+            provisional.items.length > 0 && provisionalExtract.push(provisional);
+        });
+
+    statusItems &&
+        statusItems.map(statusItem => {
+            if (statusItem.selected) {
+                extractList.map(extractItem => {
+                    let provisional: IExtract;
+                    provisional = {
+                        amountTotal: extractItem.amountTotal,
+                        date: extractItem.date,
+                        items: extractItem.items.filter(item => {
+                            return useFilterCondition(statusItem.description, item);
+                        }),
+                    };
+                    provisional.items.length > 0 && provisionalExtract.push(provisional);
+                });
+            }
+        });
+    provisionalExtract && setFilteredExtractList(provisionalExtract);
+    return provisionalExtract;
+}
+
+export default useExtractFiltered;
